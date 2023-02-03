f67bb54448c0b9b3ea90d62241494fd70c891e5d
Thirhag <meigraf@hotmail.de>
2023-02-03 12:09:21 +0100

    12

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,9 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+        Child child = new Child();
+        output(child.sayHello());
+        output(child.sayHello());
     }
 
     private static void output(String outputStr) {
---
+++ com/cc/java/Parent.java
@@ -0,0 +1,14 @@
+package com.cc.java;
+
+import java.util.jar.Attributes.Name;
+
+public class Parent {
+    public String sayHello(){
+            return "Hello from Parent!";
+    }
+
+    public String sayHello(String name){
+            return "Hello" + name + "!";          
+    
+}
+}
